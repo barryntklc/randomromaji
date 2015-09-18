0fb31e9ed5cc821e37bf67139496fd8243d71468
Chunmeista <barryn.chun@gmail.com>
2015-09-17 18:26:46 -1000

    UI change drafting
    A proposed new look for the user interface is being drafted in
    home02.html

---
+++ appearance.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Chunmeista on 9/17/2015.
+ */
+/*
+window.onload = function() {
+    var singlebutton = document.getElementsByClassName("divbutton01");
+    singlebutton.addEventListener("mousedown", function(){
+
+    });
+    singlebutton.addEventListener("mouseup", function(){
+
+    });
+
+    var allbutton = document.getElementsByClassName("divbutton00");
+    allbutton.addEventListener("mousedown");
+}
+*/
+<!--Developing a more dynamic UI for RandomRomaji-->
+
+function depress(element) {
+    element.style.backgroundColor = "#195CCE";
+}
+
+function release(element) {
+    element.style.backgroundColor = "";
+}
+
+function select(element) {
+    element.style.backgroundColor = "dodgerblue";
+}
